3490e6447c927e88129d179d9ee3afc147680eb7
Joel Kagohara Sasaki <126785821+JoelKSasaki@users.noreply.github.com>
2023-03-16 11:49:25 -0300

    Update velha.java

--- velha.java
+++ velha.java
@@ -1,46 +1,59 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class velha {
     public static void main(String[] args) {
-        int [][] tabuleiro = new int [3][3];
-        char p1[10];
-        char p2[10];
+        Scanner scan = new Scanner(System.in);
+        char [][] tab = {{'A','B','C'},{'D','E','F'},{'G','H','I'}};
+        char p1 = 'X';
+        char p2 = 'O';
 
-        for (int i = 0; i < tabuleiro.length; i++)
-        {
-            Arrays.fill(tabuleiro[i], '.');
-        }
-
-        for(int i=0;i<3;i++)
-        {
-            for (int j = 0;j < 3;j++) {
-                System.out.print(tabuleiro[i][j]);
+        System.out.println("considerando:");
+        for(int i=0;i<3;i++) {
+            for (int j=0;j<3;j++) {
+                System.out.print(tab[i][j]);
             }
             System.out.println();
         }
-        moviment(p1, p2);
+
+        mostrartab(tab);
+        moviment(p1, tab);
+        moviment(p2, tab);
+    }
+    public static void mostrartab(char[][] tabuleiro){
+
+        for (int i = 0; i < tabuleiro.length; i++)
+        {
+            Arrays.fill(tabuleiro[i], ' ');
+        }
+
+        for(int i=0;i<3;i++) {
+            for (int j=0;j<3;j++) {
+                System.out.print("|  |");
+            }
+            System.out.println();
+        }
 
     }
+    public static void moviment(char j, char[][] table) {
+        int x = 0;
+        int y = 0;
+        boolean winner = false;
 
-    public static void moviment(int j1, int j2){
-        int x;
-        int y;
-        int i;
-        int j;
-
-        for(int x=0;x<9;x++){
-            for (int i = 0; i < tabuleiro.length; i++)
-            {
-                Arrays.fill(tabuleiro[i], 'o');
-
-                for(int i=0;i<3;i++)
-                {
-                    for (int j = 0;j < 3;j++) {
-                        System.out.print(tabuleiro[i][j]);
-                    }
+        while(winner == false) {
+            table[x][y] = j;
+            System.out.println("Digite aonde deseja jogar: ");
+            for (x = 0; x < 3; x++) {
+                for (y = 0; y < 3; y++) {
+                    System.out.print(table[x][y] + j);
                 }
             }
         }
     }
+
+    public static boolean tie(){
+        return false;
+    }
 }
+
 
 
